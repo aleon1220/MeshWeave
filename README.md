57d24d57bfe61d6e4fd7bab13bacf615bc9f54da
Andres Leon <aleon1220@gmail.com>
2018-07-07 17:00:23 +1200

    Adding project. WS invokation to WSDL endpoint
    WS invocation. Adding BizTalk Project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ESB-BizTalk-WebServiceComposition
-composition of web services in biztalk server
+Simple invokation of web services in biztalk server.
+
+For Microsoft Visual Studio Solution File  Visual Studio 2010
+BizTalk Deployment Server Version=3.0.1.0
+
+Language used is **C#**
+
+obtains data from endpoint: http://nonnb-w7.dvt.co.za/BizTalkWCF.Orchs/BizTalkWCF_AddSuffixService_RcvSuffixService.svc
+invokes method GetDataUsingDataContract result is returned in XML Format
+
+By Andres Leon SOA-Integration Professional
+Master Service Oriented Computing
---
+++ WcfService/Service.svc.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Contracts;
+using System.ServiceModel;
+
+#initializes the behavior
+
+namespace WcfService
+{
+    [ServiceBehavior(Namespace = "YourNameSpaceHere")]
+    public class MyService : IService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public SomeSerializableEntity GetDataUsingDataContract(SomeSerializableEntity entity)
+        {
+            Contract.Requires(entity != null);
+
+            if (entity.BoolValue)
+            {
+                entity.StringValue += " Suffix";
+            }
+            return entity;
+        }
+    }
+}
